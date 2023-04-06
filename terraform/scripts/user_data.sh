66844944db1c2ac47d5dc78d848503d749bbda4d
Marcelo Henrique Borgas <marcelo.borgas@edu.fecap.br>
2023-04-05 22:45:18 -0300

    cm

--- terraform/scripts/user_data.sh
+++ terraform/scripts/user_data.sh
@@ -11,8 +11,8 @@ sudo systemctl enable httpd
     
 echo "Install application"
 cd /tmp
-git clone https://github.com/kledsonhugo/app-static-site-ec2
-cp /tmp/app-static-site-ec2/app/*.html /var/www/html/
+git clone https://github.com/TcheloBorgas/TcheloBorgas-app-static-ec2-tchelo.git
+cp /tmp/app-static-ec2-tchelo/*.html /var/www/html/
     
 echo "Start Apache service"
 service httpd restart
